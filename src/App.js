b4f2b11d6b3912a4a97cd404ce4a6d46bea97122
Badri Vasistha <badri.vasistha@gmail.com>
2019-12-11 22:02:58 -0800

    init repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HelloBootstrap from "./components/HelloBootstrap";
+
+
+function App() {
+  return (
+    <div>
+      <HelloBootstrap/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloBootstrap.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function HelloBootstrap() {
+  return (
+    <div className="pos-f-t">
+      <div className="collapse" id="navbarToggleExternalContent">
+        <div className="bg-dark p-4">
+          <h4 className="text-white">Collapsed content</h4>
+          <span className="text-muted">Toggleable via the navbar brand.</span>
+        </div>
+      </div>
+      <nav className="navbar navbar-dark bg-dark">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarToggleExternalContent"
+          aria-controls="navbarToggleExternalContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Fluid jumbotron</h1>
+          <p class="lead">
+            This is a modified jumbotron that occupies the entire horizontal
+            space of its parent.
+          </p>
+        </div>
+      </div>
+      <div className="card text-center">
+        <div className="card-header">Featured</div>
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+        <div className="card-footer text-muted">2 days ago</div>
+      </div>
+    </div>
+  );
+}
